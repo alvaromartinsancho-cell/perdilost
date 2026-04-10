0077b5d6b4934929ab8ac9ffe0dfa5bd2b0ed0b3
alvaromartinsancho-cell <alvaro.martin.sancho@gmail.com>
2026-04-10 16:43:20 +0200

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -5,6 +5,8 @@ export default async function handler(req, res) {
 
   try {
     const { to, subject, text } = req.body;
+    const { language } = req.body;
+    const idiomaEmail = language === 'en' ? 'en' : 'es';
     const apiKey = process.env.RESEND_API_KEY;
 
     if (!apiKey) {
